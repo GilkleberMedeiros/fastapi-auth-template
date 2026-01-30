68a5746812f1abb6feffca07e1ac5e70c4db3792
Gilkleber Medeiros <gikimedei2018@gmail.com>
2026-01-30 12:31:49 -0300

    Init practice.

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="FastAPI JWT Auth Practice", 
+)
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
